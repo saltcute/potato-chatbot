25554c198f69e3c24f1ddd0804b4c46d42151428
potatopotat0 <xuanjiap@lolicon.ac.cn>
2022-07-02 00:57:18 +0800

    update

--- src/commands/v/v.app.ts
+++ src/commands/v/v.app.ts
@@ -1,11 +1,19 @@
 import { Card, AppCommand, AppFunc, BaseSession } from 'kbotify';
+import fs from 'fs';
+import upath from 'upath';
+
+const data = JSON.parse(fs.readFileSync(upath.join(__dirname, "quotes.json"), { encoding: "utf-8", "flag": "r" }));
+
+function getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+}
 
 class V extends AppCommand {
     code = 'v'; // 只是用作标记
     trigger = 'v'; // 用于触发的文字
     intro = 'v';
     func: AppFunc<BaseSession> = async (session) => {
-        return session.quote("关注[小希小桃Channel](https://space.bilibili.com/5563350)喵，关注[小希小桃Channel](https://space.bilibili.com/5563350)谢谢喵。");
+        return session.quote(data[getRandomInt(data.length)]);
     };
 }
 
