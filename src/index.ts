354b2c9168909161717147409f2d51902edbd948
potatopotat0 <xuanjiap@lolicon.ac.cn>
2022-07-22 06:01:40 +0800

    remove bot market

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,6 @@ import { helpMenu } from 'commands/singleCommand/help.menu';
 import { hitokoto } from 'commands/singleCommand/hitokoto.app';
 import { botMenu } from 'commands/bot/bot.menu';
 
-
-if (auth.enableBotMarket) {
-    botMarketStayOnline();
-}
-
 bot.messageSource.on('message', (e) => {
     bot.logger.debug(`received:`, e);
     // 如果想要在console里查看收到信息也可以用
@@ -27,21 +22,3 @@ bot.addCommands(botMenu);
 bot.connect();
 
 bot.logger.debug('system init success');
-
-function botMarketStayOnline() {
-    axios({
-        url: 'http://bot.gekj.net/api/v1/online.bot',
-        method: "POST",
-        headers: {
-            uuid: auth.botMarketUUID
-        }
-    }).then((res) => {
-        console.log(`[${new Date().toLocaleTimeString()}] Bot Market online status updating success, remote returning: `);
-        console.log(res.data);
-        setTimeout(botMarketStayOnline, (res.data.data.onTime + 5) * 1000);
-    }).catch((e) => {
-        console.log(`[${new Date().toLocaleTimeString()}] Bot Market online status updating failed. Retring in 60 seconds. Error message: `);
-        console.log(e);
-        setTimeout(botMarketStayOnline, 60 * 1000);
-    })
-}
